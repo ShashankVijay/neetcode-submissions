5ec0c87f780355689ee143b5dea895c0573b1c35
Shashank Vijay <52514260+ShashankVijay@users.noreply.github.com>
2026-05-06 23:00:55 +0530

    Add: simplify-path - submission-2

---
+++ Data Structures & Algorithms/simplify-path/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = ['/']
+        for ch in path:
+            if not stack:
+                stack = ['/']
+            if ch == '/':
+                if stack[-1] == '/':
+                    stack.pop()
+                stack.append(ch)
+                continue
+            stack[-1] = stack[-1] + ch
+        
+        print(stack)
+        stack2 = []
+
+        for i, subPath in enumerate(stack):
+            if subPath == "/.":
+                continue
+            elif subPath == "/..":
+                if stack2:
+                    stack2.pop()
+            else:
+                stack2.append(subPath)
+
+        print(stack2)
+        if not stack2:
+            stack2.append('/')
+        if len(stack2) > 1 and stack2[-1] == '/':
+            stack2.pop()
+        return "".join(stack2)
+        
+
